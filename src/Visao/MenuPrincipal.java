379125666cb031a475fd7172029db680481381f8
rafaelbrpg <rafa.rfsiqueira@gmail.com>
2013-08-22 17:00:37 -0300

    Layout da Abertura das janelas - (Abrem no centro da Pagina e Com a Função Modal Ativa).

--- src/Visao/MenuPrincipal.java
+++ src/Visao/MenuPrincipal.java
@@ -41,7 +41,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jPanel4 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         btnBuscarCliente = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
 
         jMenu1.setText("jMenu1");
 
@@ -175,13 +174,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jButton2.setText("jButton2");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -196,9 +188,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
                         .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(31, 31, 31)
                         .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                .addGap(280, 280, 280)
-                .addComponent(jButton2))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -215,9 +204,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addGap(87, 87, 87)
-                .addComponent(jButton2)
-                .addGap(129, 129, 129))
+                .addGap(239, 239, 239))
         );
 
         jLabel1.getAccessibleContext().setAccessibleName("txtLabel");
@@ -228,34 +215,34 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
     private void btnCadastrarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarClienteActionPerformed
         CadastroCliente janelaCadastroCli = new CadastroCliente(this,true);
+        janelaCadastroCli.setLocationRelativeTo(null);
         janelaCadastroCli.setVisible(true);
                 
     }//GEN-LAST:event_btnCadastrarClienteActionPerformed
 
     private void btnCadastrarFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarFuncionarioActionPerformed
         CadastroFuncionario janelaCadastroFunc = new CadastroFuncionario(this,true);
+        janelaCadastroFunc.setLocationRelativeTo(null);
         janelaCadastroFunc.setVisible(true);
     }//GEN-LAST:event_btnCadastrarFuncionarioActionPerformed
 
     private void btnCompraVeiculosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCompraVeiculosActionPerformed
         CompraVeiculo janelaComprarVeiculo = new CompraVeiculo(this,true);
+        janelaComprarVeiculo.setLocationRelativeTo(null);
         janelaComprarVeiculo.setVisible(true);
     }//GEN-LAST:event_btnCompraVeiculosActionPerformed
 
     private void btnBuscarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarClienteActionPerformed
         BuscarCliente janelabusBuscarCliente = new BuscarCliente(this,true);
+        janelabusBuscarCliente.setLocationRelativeTo(null);
         janelabusBuscarCliente.setVisible(true);
     }//GEN-LAST:event_btnBuscarClienteActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         CadastroClientePJ janelaCadastroClientePJ = new CadastroClientePJ(this,true);
+        janelaCadastroClientePJ.setLocationRelativeTo(null);
         janelaCadastroClientePJ.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        sdsdds sd = new sdsdds(this, true);
-        sd.setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -296,7 +283,6 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton btnCompraVeiculos;
     private javax.persistence.EntityManager entityManager1;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
