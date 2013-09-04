b37831c16dab134ad17f64b6f24b5232c3a26dc0
rafaelbrpg <rafa.rfsiqueira@gmail.com>
2013-09-04 17:45:07 -0300

    CompraRn

--- src/RN/CompraRN.java
+++ src/RN/CompraRN.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -49,63 +48,4 @@ public class CompraRN {
    public void setCompraById(Integer id) {
         compra = pers.procurarPorId(id);
     }
-    
-<<<<<<< HEAD
 }
-=======
-}
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package RN;
-
-import model.Compra;
-import Persistencia.CompraPers;
-import Wrapper.CompraWrapper;
-import java.util.ArrayList;
-import java.util.List;
-/**
- *
- * @author rafael
- */
-public class CompraRN {
-   private CompraPers pers;
-
-    public CompraRN() {
-        pers = new CompraPers();
-    }
-   
-    public List<CompraWrapper> getCompraWrapperList() {
-        List<CompraWrapper> lista = new ArrayList<>();
-        for (Compra compra : pers.getLista()) {
-        //        lista.add(new CompraWrapper(compra));
-        }
-        return lista;
-    }
-    
-    public CompraPers getPers() {
-        return pers;
-    }
-
-    public void setPers(CompraPers pers) {
-        this.pers = pers;
-    }
-
-    public Compra getCompra() {
-        return compra;
-    }
-
-    public void setCompra(Compra compra) {
-        this.compra = compra;
-    }
-   private Compra compra;
-   
-   
-   
-    
-    
-}
->>>>>>> bfc06f50b6598bf6b510efddc2cf53d18c37edff
->>>>>>> 20688334c819c3c20056998ab44e60df20ae404f
